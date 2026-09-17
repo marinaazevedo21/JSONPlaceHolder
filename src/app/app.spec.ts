5519e27e1a42a2815e16f0a8df86de92eebdb0fa
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-09-17 09:59:26 -0300

    ManipulacaodeDados.APIS(POST VITEST COVERAGE)

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideHttpClient()],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('deve criar o aplicativo', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('deve renderizar o título', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, JSONPlaceHolder');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Cadastro de Tarefa');
   });
 });
