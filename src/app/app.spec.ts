184729e2e28e4ff62139283a74e8dd0defd18437
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-09-18 09:38:35 -0300

    Atualização e Exclusão com APis

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from
+'@angular/common/http/testing';
 import { App } from './app';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-      providers: [provideHttpClient()],
-    }).compileComponents();
-  });
+let httpMock: HttpTestingController;
 
-  it('deve criar o aplicativo', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+beforeEach(async () => {
+await TestBed.configureTestingModule({
+imports: [App],
+providers: [provideHttpClient(), provideHttpClientTesting()],
+}).compileComponents();
 
-  it('deve renderizar o título', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Cadastro de Tarefa');
-  });
+httpMock = TestBed.inject(HttpTestingController);
+});
+
+afterEach(() => {
+httpMock.verify();
+});
+
+it('deve criar o aplicativo', () => {
+const fixture = TestBed.createComponent(App);
+const app = fixture.componentInstance;
+expect(app).toBeTruthy();
+httpMock.expectOne((req) =>
+req.url.startsWith('https://jsonplaceholder.typicode.com/posts')).flush([]);
+});
+
+it('deve renderizar o título', async () => {
+const fixture = TestBed.createComponent(App);
+httpMock.expectOne((req) =>
+req.url.startsWith('https://jsonplaceholder.typicode.com/posts')).flush([]);
+await fixture.whenStable();
+
+const compiled = fixture.nativeElement as HTMLElement;
+expect(compiled.querySelector('h1')?.textContent).toContain('Gerenciador de Tarefas');
+});
 });
